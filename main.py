d816190f67e320b4898c964dd8eed7eb88a13492
Liminss <153162777+Liminss@users.noreply.github.com>
2024-07-13 21:55:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
     parser.add_argument('--data_dir', default='./data/', type=str)
     parser.add_argument('--output_dir', default='output/', type=str)
     parser.add_argument('--data_name', default='LastFM', type=str)
-    parser.add_argument('--do_eval', action='store_false')
+    parser.add_argument('--do_eval', action='store_true')
     parser.add_argument('--similarity_model_name', default='ItemCF_IUF', type=str, 
                         help="Method to generate item similarity score. choices: ItemCF, ItemCF_IUF(Inverse user frequency)")
 
